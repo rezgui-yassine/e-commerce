13f4a1c253742b9e816638c3fa2684437af0f257
yassine <rezguiyassine224@gmail.com>
2025-01-16 12:04:15 +0100

    Initial commit

---
+++ ecommerce/ecommerce/src/test/java/com/yassineCoding/ecommerce/EcommerceWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassineCoding.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
