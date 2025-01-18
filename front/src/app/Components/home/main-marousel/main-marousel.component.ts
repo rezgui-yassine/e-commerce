0b46128eaba32bc6481c16d964df87be875ae893
yassine <rezguiyassine224@gmail.com>
2025-01-18 10:43:07 +0100

    add navar , footer , mainHome components

---
+++ front/src/app/Components/home/main-marousel/main-marousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-marousel',
+  templateUrl: './main-marousel.component.html',
+  styleUrls: ['./main-marousel.component.scss']
+})
+export class MainMarouselComponent {
+
+}
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Layouts/navbar/navbar.component';
+import { FooterComponent } from './Layouts/footer/footer.component';
+import { MainMarouselComponent } from './Components/home/main-marousel/main-marousel.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainMarouselComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
